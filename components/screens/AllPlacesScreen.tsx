50c88338042560d10b130d03fb5f4c7cd26bb6f1
신승훈 <zlzlzlmo@daum.net>
2022-07-26 21:42:39 +0900

    first commit

---
+++ components/screens/AllPlacesScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AllPlacesScreen = () => {
+  return (
+    <View>
+      <Text>AllPlacesScreen</Text>
+    </View>
+  );
+};
+
+export default AllPlacesScreen;
+
+const styles = StyleSheet.create({});
---
+++ components/ui/places/PlaceList.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PlaceList = () => {
+  return (
+    <View>
+      <Text>PlaceList</Text>
+    </View>
+  );
+};
+
+export default PlaceList;
+
+const styles = StyleSheet.create({});
