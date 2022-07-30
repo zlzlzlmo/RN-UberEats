14563ca0b9090cbabae29e822ca747dee1909942
신승훈 <zlzlzlmo@daum.net>
2022-07-31 07:59:17 +0900

    feat: add restaurant iamge

--- components/ui/restaurant/RestaurantItem.tsx
+++ components/ui/restaurant/RestaurantItem.tsx
@@ -1,11 +1,13 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { LOCAL_RESTAURANTS } from "../../data/constants/restaurants";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { Colors } from "../../styles/colors";
 
 const RestaurantItem = () => {
   return (
     <View>
-      <Text>RestaurantItem</Text>
+      <RestaurantImage />
     </View>
   );
 };
@@ -15,5 +17,17 @@ export default RestaurantItem;
 const styles = StyleSheet.create({});
 
 const RestaurantImage = () => (
-  <Image source={{ uri: LOCAL_RESTAURANTS[0].image_url }} />
+  <>
+    <Image
+      source={{ uri: LOCAL_RESTAURANTS[0].image_url }}
+      style={{ width: "100%", height: 180 }}
+    />
+    <TouchableOpacity style={{ position: "absolute", right: 20, top: 20 }}>
+      <MaterialCommunityIcons
+        name="heart-outline"
+        size={25}
+        color={Colors.white}
+      />
+    </TouchableOpacity>
+  </>
 );
