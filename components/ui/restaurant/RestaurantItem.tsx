7e7a26e9b990f7279338fbc33e64d57308e77a19
신승훈 <zlzlzlmo@daum.net>
2022-07-31 08:05:12 +0900

    style: add margin, padding to restaurant item

--- components/ui/restaurant/RestaurantItem.tsx
+++ components/ui/restaurant/RestaurantItem.tsx
@@ -2,10 +2,11 @@ import { StyleSheet, View } from "react-native";
 import React from "react";
 import RestaurantInfo from "./RestaurantInfo";
 import RestaurantImage from "./RestaurantImage";
+import { Colors } from "../../styles/colors";
 
 const RestaurantItem = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <RestaurantImage />
       <RestaurantInfo name="Restaurant Name" rating={4} />
     </View>
@@ -14,4 +15,10 @@ const RestaurantItem = () => {
 
 export default RestaurantItem;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    padding: 15,
+    backgroundColor: Colors.white,
+  },
+});
